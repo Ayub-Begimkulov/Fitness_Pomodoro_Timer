8798e70884782840e09462f8a926cd1f2ffb6b92
Ayub-Begimkulov <begimkulov963@gmail.com>
2019-07-07 16:54:58 +0300

    Update postcss.config.js
    Chaged from object syntax to array and added config file

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,8 @@
+const tailwindcss = require('tailwindcss')
+
 module.exports = {
-  plugins: {
-  	tailwindcss: require('tailwindcss'),
-    autoprefixer: {}
-  }
+  plugins: [
+  	tailwindcss('./tailwind.config.js'),
+    require('autoprefixer')
+  ]
 }
