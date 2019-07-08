92e5bb2bba6323717b0ef07880686af73992a853
Ayub-Begimkulov <begimkulov963@gmail.com>
2019-07-08 11:27:10 +0300

    Update postcss.config.js
    Added extractor config to postcss

--- postcss.config.js
+++ postcss.config.js
@@ -5,7 +5,18 @@ module.exports = {
   plugins: [
   	tailwindcss('./tailwind.config.js'),
   	purgecss({
-  		content: ['./public/index.html', './src/**/*.vue']
+  		content: ['./public/index.html', './src/**/*.vue'],
+  		extractors: [
+  			{
+  				extractor: class TailwindExtractor {
+  					static extract(content) {
+  						return content.match(/[A-z0-9-:\/]+/g) || [];
+  					}
+  				},
+
+  				extensions: ['html', 'vue']
+  			}
+			]
   	}),
     require('autoprefixer')
   ]
